ab9863a4b01cd519f33fefe7847d84e3bc0e794e
giannivitale <giavitale78@gmail.com>
2025-12-12 11:26:47 +0100

    Improve movie display structure in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -39,10 +39,10 @@ export default function HomePage() {
        
        <section className="my4 py-3">
         <div className="container">
-            <div className="row">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                 {
                   movies.map(movie =>
-                    <div className="col">
+                    <div className="col" key={movie.id}>
                         <div className="card">
                               <img src={movie.image} alt="" className="card-img-top" />
                                 <div className="card-body">
