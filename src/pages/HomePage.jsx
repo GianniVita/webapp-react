7af33771f597b889cdd01c6e9af9c14e2a9f47e4
giannivitale <giavitale78@gmail.com>
2025-12-12 09:15:43 +0100

    fetch movies data from API in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,30 @@
+import axios from "axios"
+import { useEffect,useState } from "react"
+
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([])
+
+
+    useEffect(()=>{
+
+        axios.get('http://localhost:3000/api/movies')
+        .then(response =>{
+            console.log(response);
+            
+        }).catch(err =>{
+            console.log(err.message);
+            
+        })
+        
+
+    }, [])
+
+
+
+
+
+
     return (
         <div className="text-center">
             <h1>Welcome to our Movie reviews app</h1>
