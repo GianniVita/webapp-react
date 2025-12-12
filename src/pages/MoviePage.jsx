17111f5851373d5a8eff3ca631a5c21d76e86d58
giannivitale <giavitale78@gmail.com>
2025-12-12 16:44:38 +0100

    Fix optional chaining for movie properties in MoviePage component

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,18 +1,35 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function MoviePage() {
 
     const {id} = useParams();
+    const [movie, setMovies] = useState();
+
+
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/movies/${id}`)
+            .then(response => {
+                console.log(response);
+                setMovies(response.data)
+            })
+            .catch(error => {
+                console.error("There was an error fetching the movie data!", error);
+            });    
+    }, []);
 
     return(
 
         <div>
-            <img src={`/images/movies/${movie.image}`} alt={movie.title}className=""/>
+            <img src={`/images/movies/${movie?.image}`} alt={movie.title}className=""/>
             
                 <h3 className="">
-                    {movie.title}
+                    {movie?.title}
                 </h3>
-                <p>{movie.abstract}</p>
+                <p>{movie?.abstract}</p>
         </div>
     )
 }
